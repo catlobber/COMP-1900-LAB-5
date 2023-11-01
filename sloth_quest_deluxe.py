e9b6b66515713447802bd28c919f2528130ae218
catlobber <themogmog360@gmail.com>
2023-11-01 13:27:16 -0500

    woop

--- sloth_quest_deluxe.py
+++ sloth_quest_deluxe.py
@@ -6,8 +6,41 @@ print("")
 print("You're currently at position (0,0) in the world.")
 print("Available commands: ")
 print("N - go north, S- go south, E - go east, W - go west, X - exit game")
-#initialize game variable
+#initialize game variable and statistics variables
 game = 1
+abandoned = 0
+lost = 0
+won = 0
+avgwin = 0.0
+maxwin = 0
+minwin = 0
+gamesplayed = 0
+totalmovement = 0
+#create stats function to display game stats
+def stats():
+  global maxwin
+  global minwin
+  global avgwin
+  #For the first win, min and max are the same but after the first win check the max and minimum moves to win and change them
+  if won == 1:
+     maxwin = movement
+     minwin = movement
+  elif won > 1:
+     if maxwin < movement:
+         maxwin = movement
+     if minwin > movement:
+         minwin = movement
+  avgwin = (totalmovement / won)
+  print("Your Statistics So Far")
+  print("----------------------")
+  print(f"Games Won: {won}/{gamesplayed}")
+  print(f"Games Lost: {lost}/{gamesplayed}")
+  print(f"Games Abandoned: {abandoned}/{gamesplayed}")
+  print(f"Average moves to win: {avgwin}")
+  print(f"Minimum moves to win: {minwin}")
+  print(f"Maximum moves to win: {maxwin}")
+
+ 
 #create restartgame function to reset all game variables
 def restartgame():
 #Create goal and hazard coordinate
@@ -45,8 +78,6 @@ print("")
 print("You're currently at position (0,0) in the world.")
 print("Available commands: ")
 print("N - go north, S- go south, E - go east, W - go west, X - exit game")
-
-
 #While Loop, checking for user inputs to see where they want to move and checking for if they reached the hazard/goal. If either is reached, the game ends and text is shown.
 #Also, add a counter variable for moves and print them
 while game == 1:
@@ -68,6 +99,8 @@ while game == 1:
      userX -= 1
      print(f"Moving west to {userX}, {userY} [moves taken: {movement}]")
     elif direction == "X":
+      abandoned += 1
+      gamesplayed += 1
       break
     else: 
        print("Learn to read.")
@@ -76,14 +109,22 @@ while game == 1:
        print("You  feel good about this.")
     if ((math.sqrt((xHaz - userX)**2 + ((yHaz - userY)**2)) < 2)):
        print("You don't feel too good.")
+    #Print win/loss statements and display statistics
     if (userX == xGoal and userY == yGoal):
        print("You reached Slothtopia!")
+       won += 1
+       gamesplayed += 1
+       totalmovement += movement
+       stats()
        game = int(input("Play again? Type 1 for yes, 2 for no."))
        if game == 1:
          restartgame()
          print("You're currently at position (0,0) in the world.")
     if (userX == xHaz and userY == yHaz):
        print("You've been captured by anti-sloth protestors!.")
+       lost += 1
+       gamesplayed += 1
+       stats()
        game = int(input("Play again? Type 1 for yes, 2 for no."))
        if game == 1:
          restartgame()
